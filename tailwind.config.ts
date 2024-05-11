5cafaa0b94865c46734cc392dc1c3c73b9e4b2ba
photonrays <lthang052@gmail.com>
2024-05-11 19:25:44 +0700

    initialize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'primary': '#552A92',
+        'secondary': '#8F7003',
+        'black': '#0e0e0e',
+        'gray-light': '#666666',
+        'gray': '#1C1C1C',
+        'text-white': '#AAAAAA',
+        'text-gray': '#515151',
+        'custom': '#3c4858',
+      },
     },
   },
   plugins: [],
